2095212bfc67f37b17a6109ba7ace39e0c96c4c0
Benjamin Goldberg <ben@spothero.com>
2019-07-18 20:34:37 -0500

    comments and enable docker

--- workspace/configure_workspace.sh
+++ workspace/configure_workspace.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# Docker
+apt-get remove docker docker-engine docker.io containerd runc
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install -y docker-ce
+systemctl enable docker
+
+# Docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
