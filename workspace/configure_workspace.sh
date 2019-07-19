17a141c313df4e1f00b2dd5127f986414b1ca96e
Benjamin Goldberg <ben@spothero.com>
2019-07-18 20:29:25 -0500

    oops

--- workspace/configure_workspace.sh
+++ workspace/configure_workspace.sh
@@ -4,5 +4,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install -y docker-ce
-curl -L sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
