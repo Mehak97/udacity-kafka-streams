c2890612195de87d0197df14414143b84a24ab67
Benjamin Goldberg <ben@spothero.com>
2019-08-04 07:57:38 -0500

    exercise 6

---
+++ classroom/samples/lesson2/sample6.py
@@ -0,0 +1,81 @@
+import asyncio
+from dataclasses import dataclass, field
+import json
+import random
+
+from confluent_kafka import Consumer, Producer
+from confluent_kafka.admin import AdminClient, NewTopic
+from faker import Faker
+
+
+faker = Faker()
+
+BROKER_URL = "PLAINTEXT://localhost:9092"
+
+
+async def produce(topic_name):
+    """Produces data into the Kafka Topic"""
+    p = Producer({"bootstrap.servers": BROKER_URL})
+    while True:
+        p.produce(topic_name, Purchase().serialize())
+        await asyncio.sleep(.01)
+
+
+async def consume(topic_name):
+    """Consumes data from the Kafka Topic"""
+    c = Consumer({
+        "bootstrap.servers": BROKER_URL,
+        "group.id": "0"
+    })
+    c.subscribe([topic_name])
+    while True:
+        #
+        # TODO: Modify the polling time and see how it changes the consumption loop
+        #
+        #
+        # TODO: Try using consume instead of poll
+        #
+        message = c.poll(1.0)
+        if message is None:
+            print("no message received by consumer")
+        elif message.error() is not None:
+            print(f"error from consumer {message.error()}")
+        else:
+            print(f"consumed message {message.key()}: {message.value()}")
+        await asyncio.sleep(.01)
+
+
+def main():
+    """Checks for topic and creates the topic if it does not exist"""
+    client = AdminClient({"bootstrap.servers": BROKER_URL})
+
+    try:
+        asyncio.run(produce_consume("com.udacity.lesson2.sample6.purchases"))
+    except KeyboardInterrupt as e:
+        print("shutting down")
+
+
+async def produce_consume(topic_name):
+    """Runs the Producer and Consumer tasks"""
+    t1 = asyncio.create_task(produce(topic_name))
+    t2 = asyncio.create_task(consume(topic_name))
+    await t1
+    await t2
+
+
+@dataclass
+class Purchase:
+    username: str = field(default_factory=faker.user_name)
+    currency: str = field(default_factory=faker.currency_code)
+    amount: int = field(default_factory=lambda: random.randint(100, 200000))
+
+    def serialize(self):
+        return json.dumps({
+            "username": self.username,
+            "currency": self.currency,
+            "amount": self.amount,
+        })
+
+
+if __name__ == "__main__":
+    main()
