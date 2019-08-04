b56e4804ca6976561e354df7b4d1d7d6730a011c
Benjamin Goldberg <ben@spothero.com>
2019-08-03 21:31:49 -0500

    exercise 3

---
+++ classroom/samples/lesson2/sample3.py
@@ -0,0 +1,98 @@
+import asyncio
+from dataclasses import dataclass, field
+import json
+import random
+
+from confluent_kafka import Consumer, Producer
+from confluent_kafka.admin import AdminClient, NewTopic
+from faker import Faker
+
+
+faker = Faker()
+
+BROKER_URL = "PLAINTEXT://localhost:9092"
+TOPIC_NAME = "org.udacity.sample3.purchases"
+
+
+async def produce_sync(topic_name):
+    """Produces data synchronously into the Kafka Topic"""
+    p = Producer({"bootstrap.servers": BROKER_URL})
+
+    curr_iteration = 0
+    while True:
+        p.produce(topic_name, Purchase(producer_type="synchronous").to_json())
+        p.flush()
+
+
+async def produce_async(topic_name):
+    """Produces data asynchronously into the Kafka Topic"""
+    p = Producer({"bootstrap.servers": BROKER_URL})
+    while True:
+        p.produce(topic_name, Purchase(producer_type="asynchronous").to_json())
+
+
+def main():
+    """Checks for topic and creates the topic if it does not exist"""
+    create_topic(TOPIC_NAME)
+    try:
+        asyncio.run(produce_consume(TOPIC_NAME))
+    except KeyboardInterrupt as e:
+        print("shutting down")
+
+
+async def consume(topic_name):
+    """Consumes data from the Kafka Topic"""
+    c = Consumer({"bootstrap.servers": BROKER_URL, "group.id": "0"})
+    c.subscribe([topic_name])
+    while True:
+        message = c.poll(1.0)
+        if message is None:
+            print("no message received by consumer")
+        elif message.error() is not None:
+            print(f"error from consumer {message.error()}")
+        else:
+            print(f"consumed message {message.key()}: {message.value()}")
+
+
+async def produce_consume(topic_name):
+    """Runs the Producer and Consumer tasks"""
+    t1 = asyncio.create_task(produce_sync(topic_name))
+    t2 = asyncio.create_task(produce_async(topic_name))
+    #t3 = asyncio.create_task(consume(topic_name))
+    await t1
+    await t2
+    #await t3
+
+
+def create_topic(client):
+    """Creates the topic with the given topic name"""
+    client = AdminClient({"bootstrap.servers": BROKER_URL})
+    futures = client.create_topics(
+        [NewTopic(topic=TOPIC_NAME, num_partitions=5, replication_factor=1)]
+    )
+
+    for _, future in futures.items():
+        try:
+            future.result()
+        except Exception as e:
+            pass
+
+
+@dataclass
+class Purchase:
+    producer_type: str
+    username: str = field(default_factory=faker.user_name)
+    currency: str = field(default_factory=faker.currency_code)
+    amount: int = field(default_factory=lambda: random.randint(100, 200000))
+
+    def to_json(self):
+        return json.dumps({
+            "type": self.producer_type,
+            "username": self.username,
+            "currency": self.currency,
+            "amount": self.amount,
+        })
+
+
+if __name__ == "__main__":
+    main()
